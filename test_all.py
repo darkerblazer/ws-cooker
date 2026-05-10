2441e97808fd5ed28958489de7d8e31cbc3f7013
darkerblazer <darkblazemaster@proton.me>
2026-05-10 23:11:05 +0900

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,6 +1,6 @@
 import os
 import importlib
 
-print([ importlib.import_module(i[:-3]) for i in os.listdir() if i.endswith(".py") and not i.startswith("test_") ])
+for i in [ importlib.import_module(i[:-3]) for i in os.listdir() if i.endswith(".py") and not i.startswith("test_") ]: globals()[i__name__] = i
 
 # exit(0)
