26966533f135193ac749287e181972f5dab45b44
darkerblazer <darkblazemaster@proton.me>
2026-05-10 22:59:26 +0900

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,4 +1,6 @@
-import wscooker-gui
-import stupid
+import os
+import importlib
+
+print([ importlib.import_module(i) for i in os.listdir() if i.endswith(".py") and not i.startswith("test_") ])
 
 exit(0)
