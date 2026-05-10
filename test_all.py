17893b3976ef89fa6c24002c67e17be4cc6dd17b
darkerblazer <darkblazemaster@proton.me>
2026-05-10 23:01:29 +0900

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,6 +1,6 @@
 import os
 import importlib
 
-print([ importlib.import_module(i) for i in os.listdir() if i.endswith(".py") and not i.startswith("test_") ])
+print([ importlib.import_module(i[:-3]) for i in os.listdir() if i.endswith(".py") and not i.startswith("test_") ])
 
 exit(0)
